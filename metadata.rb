b90cd3cf5b66edda40a869fcb989bdb5b5c20ee3
Lucas Mariani <lmariani@lojack.com.ar>
2014-07-01 10:33:44 -0300

    Complete metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'msodbcsql'
-maintainer       ''
-maintainer_email ''
-license          ''
+maintainer       'Mariani Lucas'
+maintainer_email 'marianilucas@gmail.com'
+license          'Apache 2.0'
 description      'Installs/Configures msodbcsql'
-long_description 'Installs/Configures msodbcsql'
-version          '0.1.0'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
 
 depends 'build-essential'
 depends 'yum-epel'
